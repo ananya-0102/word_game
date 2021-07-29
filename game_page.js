fe565d50628348eea9c8fa0f7d5c45501bb622fd
ananya-0102 <ananya.0102@gmail.com>
2021-07-29 12:42:29 +0530

    Update game_page.js
    Calculating players' score updated

--- game_page.js
+++ game_page.js
@@ -55,11 +55,11 @@ function check() {
 
     if (answer == word) {
         if(answer_turn == "player_1") {
-            player1_score += 1;
+            player1_score = player1_score + 1;
             document.getElementById("player1_score").innerHTML = player1_score;
         }
         else {
-            player2_score += 1;
+            player2_score = player2_score - 1;
             document.getElementById("player2_score").innerHTML = player2_score;
         }
     }
